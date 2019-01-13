b23348b0e167a12b24c285835c7b3440c5f81350
Fran <francisco_ff@hotmail.com>
2019-01-13 22:41:41 +0000

    Created a dynamic search engine when typing

--- js/script.js
+++ js/script.js
@@ -16,28 +16,13 @@ FSJS project 2 - List Filter and Pagination
    will only be used inside of a function, then it can be locally
    scoped to that function.
 ***/
-const studentList = document.querySelectorAll('.student-item');
+let studentList = document.querySelectorAll('.student-item');
 const studentPages = Math.ceil(studentList.length / 10);
 let pageList = document.querySelector('.pagination');
 const divPage = document.querySelector('div.page');
 let activePage = 1;
 
 
-
-/***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
-
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
-***/
 const showPage = (activePage) => {
   for (let i = 0; i < studentList.length; i++) {
     studentList[i].style.display = 'none';
@@ -47,14 +32,10 @@ const showPage = (activePage) => {
     }
   }
 };
-
 showPage (activePage);
 
-/***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
-***/
-const appendPageLinks = (studentPages) => {
+
+const appendPageLinks = () => {
   pageList = document.createElement('ol');
   pageList.classList.add('pagination');
 
@@ -84,7 +65,6 @@ const appendPageLinks = (studentPages) => {
 
   divPage.appendChild(pageList);
 }
-
 appendPageLinks ();
 
 
@@ -96,36 +76,24 @@ searchInput.setAttribute('placeholder', 'Search for students...');
 searchBar.appendChild(searchInput);
 document.querySelector('.page-header').appendChild(searchBar);
 
-let userInput = '';
+searchInput.addEventListener ('keyup', (foundStudents) => {
 
-searchInput.addEventListener ('keyup', () => {
-  if (searchInput.value !== '') {
-    pageList.style.display = 'none';
+  let userInput = searchInput.value.toLowerCase();
+  if (userInput !== '') {
+    // studentsList;
     for (let i = 0; i < studentList.length; i++) {
       studentList[i].style.display = 'none';
+      pageList.style.display = 'none';
+      let studentName = document.querySelectorAll('.student-details h3')[i].innerText;
+      if (studentName.includes(userInput)) {
+        studentList[i].style.display = 'block';
+      }
     }
   } else {
     showPage (activePage);
+    appendPageLinks ();
   }
 }, false);
-//   // userInput += input.key;
-//   if ( input.code >= '' ){
-//     userInput += input.key;
-//     console.log(userInput);
-//  if (input.key == 'Backspace') {
-//     console.log('BACKSPACE');
-//   }
-//   // else if (input.keyCode == 8) {
-//   //     userInput -= userInput.substring(0, userInput.length - 1);
-//   //     console.log(userInput);
-//   //   } else {
-//   //   }
-//   // } else {
-//   //   showPage (activePage)
-//   // }
-// }, false);
-
-// const searchStudents = (userInput) => {}
 
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
